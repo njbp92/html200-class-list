ab9cfbb0498e19ee2f1c5aa90b37ff839cad1279
Nataly <njbp92@gmail.com>
2019-04-23 10:26:52 -0700

    Add files via upload

--- Lesson03/excercise-three.js
+++ Lesson03/excercise-three.js
@@ -1,11 +1,5 @@
-let colors= [['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'], ['1', '2', '3', '4', '5', '6','7']];
+let colors= ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
   for (let n=0; n < colors.length; n++) {
-    console.log(colors);
-       for (let b=0; b < colors[n].length; b++) {
-        console.log(colors[n],[b]);
-      }
-    
-  }
-  const rainbow = colors.split('[]')
-console.log(rainbow);
+    console.log( n + "," + colors[n]);
+  } 
